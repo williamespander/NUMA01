38bf7a425d64bfbf08d0a3a9e8a11f7208793bb5
William Espander Jansson <81936287+williamespander@users.noreply.github.com>
2021-05-22 18:55:23 +0200

    Add files via upload

--- Birds.py
+++ Birds.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 18:04:50 2021
+
+@author: willi
+"""
+
 import datetime
 import pytz
+from matplotlib.pyplot import *
 
 class Birds:
     def __init__(self, textfile):
@@ -7,22 +15,37 @@ class Birds:
         with open(textfile, "r") as txt:
             for line in txt:
                 try:
-                    self.data.append([pytz.utc.localize(datetime.datetime.strptime(line.split(sep="  ",maxsplit=1)[0], '%Y-%m-%d %H:%M:%S.%f')).astimezone(pytz.timezone('Europe/Stockholm')),int(line.split(sep="  ", maxsplit=1)[1])])
+                    self.data.append([pytz.utc.localize(datetime.
+                                                        datetime.strptime
+                                                        (line.split(sep="  ",
+                                                        maxsplit=1)
+                                                        [0], 
+                                                        '%Y-%m-%d %H:%M:%S.%f'))
+                                      .astimezone(pytz.timezone
+                                                  ('Europe/Stockholm'))
+                                      ,int(line.split(sep="  ", maxsplit=1)
+                                           [1])])
                 except ValueError:
                     pass
-    def Step1(self): #ändrar fel element där antalet blir ett fel värde som 2172 072 2172
-        d1=0
-        for i, element in enumerate(self.data[75100:76100]): #fixa nollställningen?
-            try:
-                if element[1] > self.data[i+1][1] and element[1] <= self.data[i+2][1]: #för större säkerhet, öka till i+5 och kör while-loop.
-                    self.data[i+1][1]=element[1]
-                #if element[1] > self.data[i+1][1] and element[1] > self.data[i+2][1] :
-                #    self.data[i + 1][1] = element[1] + self.data[i + 1][1] - d1
-                #    if self.data[i+1][1]-d1 > 0:
-                #        d1=self.data[i+1][1]-d1
-            except IndexError:
-                pass
-            print(i, element, d1)
+    # def Step1(self): #ändrar fel element där antalet blir ett fel värde som 2172 072 2172
+    #     d1=0
+    #     for i, element in enumerate(self.data[75100:76100]): #fixa nollställningen?
+    #         try:
+    #             if element[1] > self.data[i+1][1] and element[1] <= self.data[i+2][1]: #för större säkerhet, öka till i+5 och kör while-loop.
+    #                 self.data[i+1][1]=element[1]
+    #             #if element[1] > self.data[i+1][1] and element[1] > self.data[i+2][1] :
+    #             #    self.data[i + 1][1] = element[1] + self.data[i + 1][1] - d1
+    #             #    if self.data[i+1][1]-d1 > 0:
+    #             #        d1=self.data[i+1][1]-d1
+    #         except IndexError:
+    #             pass
+    #         print(i, element, d1)
+    
+    # def Step1(self):
+    #     for i in range(len(self.data)):
+    #         if self.data[i][1]
+            
+        
 
 
     def Step2(self): #ta bort datum som går backåt i tiden + lägger in tider för rader som ej finns
@@ -30,23 +53,30 @@ class Birds:
         for i, element in enumerate(self.data):
             try:
                 if element[0]+dt < self.data[i+1][0]:
-                    self.data.insert(i+1, (element[0]+dt,element[1]))
+                    self.data.insert(i+1, [element[0]+dt,element[1]])
                 while element[0] > self.data[i+1][0]:
                     del self.data[i+1]
             except IndexError:
                 pass
-            print(i, element)
+            # print(i, element)
 
     def Step3(self):
         for i, element in enumerate(self.data):
             try:
-                 if element[1]+8 <= self.data[i+1][1]:
-                      self.data[i][1]=(element[1]+8)
+                  if element[1]+8 <= self.data[i+1][1]:
+                      self.data[i][1]=element[1]+8
             except IndexError:
                 pass
 
-BirdsData = Birds("/Users/williamkaul/Downloads/bird_jan25jan16.txt")
-BirdsData.Step1()
+BirdsData = Birds("bird_jan25jan16.txt")
+BirdsData.Step2()
+BirdsData.Step3()
+
+
+x_values = [BirdsData.data[i][0] for i in range(300, 2000)]
+y_values = [(BirdsData.data[i][1] - BirdsData.data[i-1][1]) for i in range(300, 2000)]
+plot(x_values, y_values)
+show()
 
 #ställen där de går bakåt i tiden
 #2016-01-12 22:18:05.547619    0
